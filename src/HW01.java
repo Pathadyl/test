8e0e2748d3fc3b7d03e94c8c6bb12f066326dfdb
Dylan <122667165+Pathadyl@users.noreply.github.com>
2025-03-16 00:18:02 +0700

    chore: Update HW01

--- src/HW01.java
+++ src/HW01.java
@@ -2,6 +2,7 @@ import org.w3c.dom.ls.LSOutput;
 
 public class HW01 {
     public static void main(String[] args) {
-        System.out.println("Đây là bài tập 1");
+        System.out.println("Đây là bài tập 01");
+        System.out.println("Updated!");
     }
 }
